32d434dff0721326ca71c08b31d7dbb69a88c5eb
Taiwo O. Adetiloye <taiwotman@users.noreply.github.com>
2020-04-30 10:16:39 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+"""App configuration."""
+from os import environ
+import os
+
+class Config:
+    """Set Flask configuration vars from .env file."""
+
+    # General Config
+
+    SECRET_KEY = os.urandom(24) #environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_ENV = environ.get('FLASK_ENV')
+
+    # Flask-Assets
+    LESS_BIN = environ.get('LESS_BIN')
+    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
+    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+
+    # Static Assets
+    STATIC_FOLDER = environ.get('STATIC_FOLDER')
+    TEMPLATES_FOLDER = environ.get('TEMPLATES_FOLDER')
+    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
+
+    # Flask-SQLAlchemy
+    # SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
